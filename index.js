185da6e3941f46fc287e306f710a7025c67062f2
Gustavo <gustavosfischer@gmail.com>
2025-09-17 21:24:17 -0300

    fix/otimizando

--- index.js
+++ index.js
@@ -11,43 +11,42 @@ const porta = 9999
 
 app.get('/exercicio1', (req, res) => {
     res.status(200)
-    const num1 = parseFloat(req.query.num1)
-    const num2 = parseFloat(req.query.num2)
-    const result = num1 + num2
-    res.send(`Resultado da soma de ${num1} + ${num2} = ${result} `)
+    const { num1, num2 } = req.query
+    res.send({
+        result: parseFloat(num1) + parseFloat(num2)
+    })
 })
 
 app.get('/exercicio2', (req, res) => {
     res.status(200)
-    const valorHora = parseFloat(req.query.valorHora)
-    const qtdHoras = parseFloat(req.query.qtdHoras)
-    const valorFinal = valorHora * qtdHoras
-    res.send(`R$${valorHora} x ${qtdHoras} horas trabalhadas = ${valorFinal}`)
+    const { valorHora, qtdHoras } = req.query
+    res.send({ 
+        result: parseFloat(valorHora) * parseFloat(qtdHoras)
+    })
 })
 
 app.get('/exercicio3', (req, res) => {
     res.status(200)
-    const peso1 = parseFloat(req.query.peso1)
-    const peso2 = parseFloat(req.query.peso2)
-    const peso3 = parseFloat(req.query.peso3)
-    const peso4 = parseFloat(req.query.peso4)
-    const peso5 = parseFloat(req.query.peso5)
-    const media = (peso1 + peso2 + peso3 + peso4 + peso5) / 5
-    res.send(`Média: ${media}`)
+    const { peso1, peso2, peso3, peso4, peso5 } = req.query
+    res.send({
+        result: (parseFloat(peso1) + parseFloat(peso2) + parseFloat(peso3) + parseFloat(peso4) + parseFloat(peso5)) / 5
+    })
 })
 
 app.get('/exercicio4', (req, res) => {
     res.status(200)
     const celsius = parseFloat(req.query.celsius)
-    const fahrenheit = (9 * celsius + 160) / 5
-    res.send(`${celsius} graus Celsius em Fahrenheit: ${fahrenheit}`)
+    res.send({
+        result: (9 * celsius + 160) / 5
+    })
 })
 
 app.get('/exercicio5', (req, res) => {
     res.status(200)
     const milhas = parseFloat(req.query.milhas)
-    const kms = milhas * 1.60934
-    res.send(`${milhas} milhas equivalem à ${kms} Kms`)
+    res.send({
+        result: milhas * 1.60934
+    })
 })
 
 // POST ------------------------------------------------------------------------------
@@ -56,43 +55,42 @@ app.use(express.json())
 
 app.post('/post/exercicio1', (req, res) => {
     res.status(200)
-    const num1 = parseFloat(req.body.num1)
-    const num2 = parseFloat(req.body.num2)
-    const result = num1 + num2
-    res.send(`Resultado da soma de ${num1} + ${num2} = ${result} `)
+    const { num1, num2 } = req.body
+    res.send({
+        result: parseFloat(num1) + parseFloat(num2)
+    })
 })
 
 app.post('/post/exercicio2', (req, res) => {
     res.status(200)
-    const valorHora = parseFloat(req.body.valorHora)
-    const qtdHoras = parseFloat(req.body.qtdHoras)
-    const valorFinal = valorHora * qtdHoras
-    res.send(`R$${valorHora} x ${qtdHoras} horas trabalhadas = ${valorFinal}`)
+    const { valorHora, qtdHoras } = req.body
+    res.send({ 
+        result: parseFloat(valorHora) * parseFloat(qtdHoras)
+    })
 })
 
 app.post('/post/exercicio3', (req, res) => {
     res.status(200)
-    const peso1 = parseFloat(req.body.peso1)
-    const peso2 = parseFloat(req.body.peso2)
-    const peso3 = parseFloat(req.body.peso3)
-    const peso4 = parseFloat(req.body.peso4)
-    const peso5 = parseFloat(req.body.peso5)
-    const media = (peso1 + peso2 + peso3 + peso4 + peso5) / 5
-    res.send(`Média: ${media}`)
+    const { peso1, peso2, peso3, peso4, peso5 } = req.body
+    res.send({
+        result: (parseFloat(peso1) + parseFloat(peso2) + parseFloat(peso3) + parseFloat(peso4) + parseFloat(peso5)) / 5
+    })
 })
 
 app.post('/post/exercicio4', (req, res) => {
     res.status(200)
     const celsius = parseFloat(req.body.celsius)
-    const fahrenheit = (9 * celsius + 160) / 5
-    res.send(`${celsius} graus Celsius em Fahrenheit: ${fahrenheit}`)
+    res.send({
+        result: (9 * celsius + 160) / 5
+    })
 })
 
 app.post('/post/exercicio5', (req, res) => {
     res.status(200)
     const milhas = parseFloat(req.body.milhas)
-    const kms = milhas * 1.60934
-    res.send(`${milhas} milhas equivalem à ${kms} Kms`)
+    res.send({
+        result: milhas * 1.60934
+    })
 })
 
 app.listen(porta, () => {
